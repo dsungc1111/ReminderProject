9e550fecaf605976d9bac0dda479d65d8b72cd91
dsungc1111 <eotjd9808@naver.com>
2024-07-06 12:54:04 +0900

    - 다른 페이지의 리스트 구성(깃발, 삭제, 상세페이지 등 )

--- ReminderProject/ViewController/Search/SearchViewController.swift
+++ ReminderProject/ViewController/Search/SearchViewController.swift
@@ -47,10 +47,7 @@ final class SearchViewController: BaseViewController {
             make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
- 
-
 }
-
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -62,10 +59,7 @@ extension SearchViewController: UISearchBarDelegate {
         print(DataList.list)
         tableView.reloadData()
     }
-    
 }
-
-
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -75,7 +69,6 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return DataList.list.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.id, for: indexPath) as? ListTableViewCell else { return ListTableViewCell() }
         let data = DataList.list[indexPath.row]
@@ -104,5 +97,36 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+        let vc = DetailViewController()
+        vc.memoTitleLabel.text = DataList.list[indexPath.row].memoTitle
+        if DataList.list[indexPath.row].priority == "높음" {
+            vc.memoTitleLabel.text = "!!!" + DataList.list[indexPath.row].memoTitle
+        }
+        vc.memoLabel.text = DataList.list[indexPath.row].memo
+        vc.dateLabel.text = Date.getDateString(date: DataList.list[indexPath.row].date ?? Date())
+        vc.tagLabel.text = "#\(DataList.list[indexPath.row].tag ?? "")"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = UIContextualAction(style: .normal, title: "삭제") { (UIContextualAction, UIView, success: @escaping (Bool) -> Void) in
+            try! self.realm.write {
+                self.realm.delete(DataList.list[indexPath.row])
+            }
+            tableView.reloadData()
+            success(true)
+        }
+        delete.backgroundColor = .systemRed
+        let flag = UIContextualAction(style: .normal, title: "깃발") { (UIContextualAction, UIView, success: @escaping (Bool) -> Void) in
+            try! self.realm.write {
+                DataList.list[indexPath.row].isFlag.toggle()
+                self.realm.create(RealmTable.self, value: ["key" : DataList.list[indexPath.row].key, "isFlag" : DataList.list[indexPath.row].isFlag], update: .modified)
+            }
+            tableView.reloadData()
+            success(true)
+        }
+        flag.backgroundColor = .systemYellow
+        return UISwipeActionsConfiguration(actions:[delete, flag])
+    }
 }
